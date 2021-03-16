243bd24a6d8d581902dae245321b7f442a420f46
iamernie8199 <iamernie8199@gmail.com>
2021-03-16 12:53:31 +0800

    Update factor_test2.py

--- factor_test2.py
+++ factor_test2.py
@@ -315,7 +315,7 @@ if __name__ == "__main__":
                                                                                                        bottom=thin)
                             wb_all[fa].cell(row=row, column=column + 1, value=mdd_diff).border = Border(top=thin,
                                                                                                         bottom=thin)
-                            wb_all[fa].cell(row=row, column=column + 2, value=mdd_diff).border = Border(top=thin,
+                            wb_all[fa].cell(row=row, column=column + 2, value=hazard_diff).border = Border(top=thin,
                                                                                                         bottom=thin)
                             fill(row, column, profit_diff, wb_all[fa])
                             fill(row, column + 1, mdd_diff, wb_all[fa])
